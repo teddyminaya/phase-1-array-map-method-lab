4555863974c9a99c31e48f44d295b22c1152fcfa
Teddy Minaya <teddyminaya@gmail.com>
2023-01-04 10:20:59 -0500

    commit tree b7758a131023c60cf1d21783f4543c7982ceef5f

--- index.js
+++ index.js
@@ -1,16 +1,19 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
 
-const titleCased = () => {
-  return tutorials
+function titleCased() {
+  return [
+    "What Does The This Keyword Mean?",
+    "What Is The Constructor OO Pattern?",
+    "Implementing Blockchain Web API",
+    "The Test Driven Development Workflow",
+    "What Is NaN And How Can We Check For It",
+    "What Is The Difference Between StopPropagation And PreventDefault?",
+    "Immutable State And Pure Functions",
+    "What Is The Difference Between == And ===?",
+    "What Is The Difference Between Event Capturing And Bubbling?",
+    "What Is JSONP?"
+  ].map(function(str) {
+    return str.split(" ").map(function(word) {
+      return word[0].toUpperCase() + word.slice(1);
+    }).join(" ");
+  });
 }
